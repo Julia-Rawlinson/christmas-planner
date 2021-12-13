be3af2ea3df9766638514ee2522ae6e8a62d2ac7
Navya Vohra <vohrana@shernet.sheridancollege.ca>
2021-12-12 22:39:08 -0500

    Created web api where asp.net's new project extention is linked. Then 2 controllerd of mvc are made. Mvc controller and web api controller. In Web Api controller: it provides the authority to add gifts in list. Then a form is made using index of java script: where authority of adding user and adding gifts is given

---
+++ WebSecurity/Controllers/NewFolder/UserApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebSecurity.Models;
+using System.Threading.Tasks;
+
+namespace WebSecurity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        UserApi[] uservalue = new UserApi[]
+        {
+            new UserApi{UserId = 001,UserName ="Julia", UserPassword=""},
+            new UserApi{UserId = 002,UserName ="Rita", UserPassword=""},
+            new UserApi{UserId = 003,UserName ="Nikita", UserPassword=""},
+            new UserApi{UserId = 004,UserName ="Navya", UserPassword=""},
+            new UserApi{UserId = 005,UserName ="Mehboob", UserPassword=""},
+        };
+
+        public IEnumerable<UserApi> GetUserApIs()
+        {
+            return uservalue;
+        }
+
+    }
+}
---
+++ Webapi/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Webapi.Controllers
+{
+    public class WebController : ApiController
+    {
+        private static List<string> addgifts = new List<string> { "socks", "chocolates", "shoes", "candycane", "flowers" };
+        
+        [HttpGet]
+        public IEnumerable<string> GetGifts()
+        {
+               return addgifts;
+        }
+        [HttpGet]
+        public string GetGiftsByIndex(int id)
+        {
+            return addgifts[2];
+        }
+        
+    }
+}
